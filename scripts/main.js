939e722faaea8be3ae4d306ebe84eec8ebcce1a3
N.S.Devaraj <nsdevaraj@gmail.com>
2016-06-05 13:01:48 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -105,8 +105,8 @@ if (!isIe) {
 window.setTimeout(setupLogo, 100);
 function setupLogo() {
   cube.rotation.set(
-      (25).degreesToRadians(),
-      (-25).degreesToRadians(),
+      (45).degreesToRadians(),
+      (-45).degreesToRadians(),
       (0).degreesToRadians()
   );
   cube.typeCubeletIds = new Array(8, 17, 16, 23, 20, 12, 21, 25);
