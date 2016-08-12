a7419d430ca5c35e4f42d5ab2d5e788e4ebbc7ec
N.S.Devaraj <nsdevaraj@gmail.com>
2016-08-12 16:13:43 +0530

    lib fonts added

--- fm/js/app.js
+++ fm/js/app.js
@@ -1,38 +1,37 @@
-var app=angular.module('tamilFm', ['ionic','ngCookies'])
+var app = angular.module('tamilFm', ['ionic', 'ngCookies'])
 
 app.config(function($ionicConfigProvider) {
-$ionicConfigProvider.tabs.position('bottom');
+  $ionicConfigProvider.tabs.position('bottom');
 })
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() { 
-  });
+  $ionicPlatform.ready(function() {});
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('home', {
-        url: '/home',
-        views: {
-            homeTab: {
-                templateUrl: 'home.html'
-            }
-        }
-    });
-    $stateProvider.state('settings', {
-        url: '/settings',
-        views: {
-            settingsTab: {
-                templateUrl: 'settings.html'
-            }
-        }
-    });
-    $stateProvider.state('favourite', {
-        url: '/favourite',
-        views: {
-            favouriteTab: {
-                templateUrl: 'favourite.html'
-            }
-        }
-    });
-    $urlRouterProvider.otherwise('/home');
+  $stateProvider.state('home', {
+    url: '/home',
+    views: {
+      homeTab: {
+        templateUrl: 'home.html'
+      }
+    }
+  });
+  $stateProvider.state('settings', {
+    url: '/settings',
+    views: {
+      settingsTab: {
+        templateUrl: 'settings.html'
+      }
+    }
+  });
+  $stateProvider.state('favourite', {
+    url: '/favourite',
+    views: {
+      favouriteTab: {
+        templateUrl: 'favourite.html'
+      }
+    }
+  });
+  $urlRouterProvider.otherwise('/home');
 })
